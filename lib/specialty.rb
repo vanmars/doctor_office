ba424d134ebee5b12e432ee6145ebafffc5f2eb2
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-06 11:22:34 -0700

    Write and PASS tests for all Specialty methods (excluding .doctors)
    Co-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>

--- lib/specialty.rb
+++ lib/specialty.rb
@@ -12,9 +12,38 @@ class Specialty
     specialties = []
     returned_specialties.each do |specialty|
       name = specialty.fetch('name')
-      id = specialty.fetch('id')
+      id = specialty.fetch('id').to_i
       specialties.push(Specialty.new({:name => name, :id => id}))
     end
     specialties
+  end
+
+  def save
+    result = DB.exec("INSERT INTO specialties (name) VALUES ('#{@name}') RETURNING id")
+    @id = result.first().fetch('id').to_i
+  end
+
+  def ==(specialty_to_compare)
+    self.name() == specialty_to_compare.name()
+  end
+
+  def self.clear
+    DB.exec('DELETE FROM specialties *;')
+  end
+
+  def self.find(id)
+    specialty = DB.exec("SELECT * FROM specialties WHERE id = #{id};").first
+    name = specialty.fetch('name')
+    id = specialty.fetch('id').to_i
+    Specialty.new({:name => name, :id => id})
+  end
+
+  def update(name)
+    @name = name
+    DB.exec("UPDATE specialties SET name = '#{name}' WHERE id = #{@id};")
+  end
+
+  def delete
+    DB.exec("DELETE FROM specialties WHERE id = #{@id};")
   end
 end 
--- spec/specialty_spec.rb
+++ spec/specialty_spec.rb
@@ -6,4 +6,52 @@ describe('#Specialty') do
       expect(Specialty.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('adds a specialty to the specialties table') do
+      spec_test = Specialty.new({:name => 'Joseph', :id => nil})
+      spec_test.save()
+      expect(Specialty.all).to(eq([spec_test]))
+    end
+  end
+
+  describe('.clear') do
+    it('deletes all specialties within the specialties table') do
+      spec_test1 = Specialty.new({:name => 'Joseph', :id => nil})
+      spec_test1.save()
+      spec_test2 = Specialty.new({:name => 'Vanessa', :id => nil})
+      spec_test2.save()
+      Specialty.clear
+      expect(Specialty.all).to(eq([]))
+    end
+  end
+
+  describe('.find') do
+    it('finds a specialty by id') do
+      spec_test1 = Specialty.new({:name => 'Joseph', :id => nil})
+      spec_test1.save()
+      spec_test2 = Specialty.new({:name => 'Vanessa', :id => nil})
+      spec_test2.save()
+      expect(Specialty.find(spec_test1.id)).to(eq(spec_test1))
+    end
+  end
+
+  describe('#update') do
+    it('updates a specialty by id') do
+      spec_test1 = Specialty.new({:name => 'Joseph', :id => nil})
+      spec_test1.save()
+      spec_test1.update('Billy')
+      expect(spec_test1.name).to(eq('Billy'))
+    end
+  end
+
+  describe('#delete') do
+    it('deletes a specialty by id') do
+      spec_test1 = Specialty.new({:name => 'Joseph', :id => nil})
+      spec_test1.save()
+      spec_test1.delete()
+      expect(Specialty.all).to(eq([]))
+    end
+  end
+
 end
